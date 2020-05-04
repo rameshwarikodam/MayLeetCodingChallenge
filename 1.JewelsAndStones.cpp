e8f40b745a420c6e71e2aa92db0ad8d61a586dda
rameshwarikodam <44030033+rameshwarikodam@users.noreply.github.com>
2020-05-04 16:23:26 +0530

    Add files via upload

---
+++ 1.JewelsAndStones.cpp
@@ -0,0 +1,62 @@
+// You're given strings J representing the types of stones that are jewels,
+// and S representing the stones you have.
+// Each character in S is a type of stone you have.
+// You want to know how many of the stones you have are also jewels.
+
+// The letters in J are guaranteed distinct, and all characters in J and S are letters.
+// Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+// Example 1:
+
+// Input: J = "aA", S = "aAAbbbb"
+// Output: 3
+// Example 2:
+
+// Input: J = "z", S = "ZZ"
+// Output: 0
+
+// Note:
+
+// S and J will consist of letters and have length at most 50.
+// The characters in J are distinct.
+#include <iostream>
+#include <string>
+using namespace std;
+class Solution
+{
+public:
+    int numJewelsInStones(string J, string S)
+    {
+        int count = 0;
+        for (int i = 0; i < J.length(); ++i)
+        {
+            for (int j = 0; j < S.length(); ++j)
+            {
+                if (J[i] == S[j])
+                    count++;
+            }
+        }
+        return count;
+    }
+};
+int main()
+{
+    int count;
+    string Jewel, Stone;
+    cin >> Jewel;
+    if (Jewel.length() > 50)
+    {
+        cout << "Please enter a string less than 50\n";
+        cin >> Jewel;
+    }
+    cin >> Stone;
+    if (Stone.length() > 50)
+    {
+        cout << "Please enter a string less than 50\n";
+        cin >> Stone;
+    }
+    //Creating object
+    Solution s;
+    count = s.numJewelsInStones(Jewel, Stone);
+    cout << count;
+}
